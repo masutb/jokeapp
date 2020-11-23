077af1c7152055a99bcd5540cf189a1752b8d083
Matus Bandur <matus@flowstudio.co>
2020-11-23 01:43:44 +0100

    add getter test

---
+++ test/Getter.spec.js
@@ -0,0 +1,44 @@
+import { getters } from "@/store/jokes.js"
+
+const list = [
+  {
+    "error": false,
+    "category": "Pun",
+    "type": "single",
+    "joke": "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false
+    },
+    "id": 206,
+    "lang": "en",
+    "isSaved": true
+  },
+  {
+    "error": false,
+    "category": "Programming",
+    "type": "twopart",
+    "setup": "Why do programmers prefer using the dark mode?",
+    "delivery": "Because light attracts bugs.",
+    "flags": {
+      "nsfw": true,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false
+    },
+    "id": 235,
+    "lang": "en"
+  }
+]
+const state = { list }
+
+describe("saved", () => {
+  test("returns poodles", () => {
+    const actual = getters.saved(state)
+    expect(actual).toEqual([ list[0] ])
+  })
+})
